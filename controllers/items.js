95ede2ef3eda0557582b57097b2903868bbd72f7
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-08 08:08:40 -0500

    start on create functionality

--- controllers/items.js
+++ controllers/items.js
@@ -15,7 +15,15 @@ function index(req, res) {
 }
 
 function create(req, res) {
-  
+  req.body.owner = req.user.profile._id
+  Item.create(req.body)
+    .then(item => {
+      res.redirect('/items')
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/items')
+    })
 }
 
 export {
