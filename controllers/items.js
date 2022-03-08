280187888bfc5fce4bda3ff699049a8333b12cdb
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-08 13:17:22 -0500

    troubleshoot new item display

--- controllers/items.js
+++ controllers/items.js
@@ -24,11 +24,11 @@ function create(req, res) {
   req.body.owner = req.user.profile._id
   Item.create(req.body)
     .then(item => {
-      res.redirect('items/index')
+      res.redirect('/items')
     })
     .catch(err => {
       console.log(err)
-      res.redirect('items/index')
+      res.redirect('/items')
     })
 }
 
