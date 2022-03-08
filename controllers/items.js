d1884f6a65a53c1692e5f6eae4a0a07bfdce7b32
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-08 09:50:44 -0500

    work on rendering after create

--- controllers/items.js
+++ controllers/items.js
@@ -24,7 +24,7 @@ function create(req, res) {
   req.body.owner = req.user.profile._id
   Item.create(req.body)
     .then(item => {
-      res.redirect('/items')
+      res.redirect('/items/index')
     })
     .catch(err => {
       console.log(err)
