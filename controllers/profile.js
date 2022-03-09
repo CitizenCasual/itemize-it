4e2927db92aadd90bcec49f1693facc089e78b12
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-09 08:28:55 -0500

    finish controller function

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,7 +3,16 @@ import { Profile } from "../models/profile.js"
 function profileView(req, res) {
   Profile.findById(req.params.id)
   .then(profile => {
-    if (profile.)
+    if (user) {
+      res.render('profile/view'), {
+        profile,
+        title: 'Account Information'
+      }
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/items`)
   })
 }
 
