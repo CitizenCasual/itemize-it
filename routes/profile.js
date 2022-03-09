ba4583d611782c98bac761f83057f949a4489bf4
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-09 10:33:41 -0500

    troubleshoot profile view

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ import * as profileCtrl from '../controllers/profile.js'
 import { isLoggedIn } from '../middleware/middleware.js'
 const router = Router()
 
-router.get('/profile', isLoggedIn, profileCtrl.profileView)
+router.get('/profile/:id', isLoggedIn, profileCtrl.profileView)
 
 export {
   router
