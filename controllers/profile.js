5e5d24be904683c52120472d134f2ef462e22ec7
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-09 10:21:09 -0500

    troubleshoot profile view

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,17 +1,14 @@
 import { Profile } from "../models/profile.js"
 
 function profileView(req, res) {
-  Profile.findById(req.params.id)
-  .then((profile) => {
-    Profile.findById(req.user.profile._id)
-    .then(self => {
-      const isSelf = self._id.equals(profile._id)
-      res.render('profile/profile', {
-        title: `${profile.name}'s Account`,
-        profile,
-        self,
-        isSelf,
-      })
+  Profile.findById(req.user.profile._id)
+  .then(self => {
+    const isSelf = self._id.equals(profile._id)
+    res.render('profile/profile', {
+      title: `${profile.name}'s Account`,
+      profile,
+      self,
+      isSelf,
     })
   })
   .catch((err) => {
