d5177e13996f4553950f9d7137a4c63c1cecf787
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-10 15:48:09 -0500

    add profile view

--- routes/profiles.js
+++ routes/profiles.js
@@ -4,6 +4,7 @@ import { isLoggedIn } from '../middleware/middleware.js'
 const router = Router()
 
 router.get('/', isLoggedIn, profileCtrl.index)
+router.get('/:id', isLoggedIn, profileCtrl.show)
 
 export {
   router
